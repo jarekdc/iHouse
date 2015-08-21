16bf4c0ec50447333aa5d48b48a4601a41e2f377
Jarekdc <jarekdc@gmail.com>
2015-08-21 19:38:24 +0200

    admin info

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,5 +1,17 @@
 var app = angular.module('iHouseApp', []);
 
 app.controller('contactCtrl', ['$scope', function($scope){
+	
+	$scope.hide = true;
+	$scope.hideOption = 'czytaj więcej';
+	$scope.hideInfo = function(){
+		$scope.hide = !$scope.hide;
+		if($scope.hide) {
+			$scope.hideOption = 'czytaj więcej';
+		} else {
+			$scope.hideOption = 'zwiń';
+		}
+	};	
+
 	$scope.hideNr = true;
 }]);
